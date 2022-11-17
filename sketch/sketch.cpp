a7590a79cde81a6713e1a655f266fe7ef34a2fd6
Daniil Ignatev <i.daniil2010@yandex.ru>
2022-11-17 19:15:01 +0100

    simple sketch + vs code

--- sketch/sketch.cpp
+++ sketch/sketch.cpp
@@ -1,12 +1,4 @@
 #include "Arduino.h"
-#include "rudiron/gpio.h"
-#include "rudiron/timer.h"
-
-using namespace Rudiron;
-
-Timer* timer_led1 = nullptr;
-
-Timer* timer_led2 = nullptr;
 
 
 void setup()
@@ -14,24 +6,17 @@ void setup()
     pinMode(LED_BUILTIN_1, OUTPUT);
     pinMode(LED_BUILTIN_2, OUTPUT);
 
+    digitalWrite(LED_BUILTIN_1, OUTPUT);
+    digitalWrite(LED_BUILTIN_2, OUTPUT);
+
     pinMode(BUTTON_BUILTIN_1, INPUT_PULLDOWN);
     pinMode(BUTTON_BUILTIN_2, INPUT_PULLDOWN);
     pinMode(BUTTON_BUILTIN_3, INPUT_PULLDOWN);
-
-    timer_led1 = Timer::getTimerForPinName(GPIO::pinMap[LED_BUILTIN_1]);
-    timer_led1->start();
-    timer_led1->PWM_setup();
-
-    timer_led2 = Timer::getTimerForPinName(GPIO::pinMap[LED_BUILTIN_2]);
-    timer_led2->start();
-    timer_led2->PWM_setup();
 
     Serial.begin(115200);
     Serial.println("РУДИРОН Бутерброд!");
 }
 
-uint8_t percentage = 0;
-bool negative = false;
 bool pressed = false;
 
 void loop()
@@ -41,25 +26,7 @@ void loop()
         Serial.println(s);
     }
 
-    if (!pressed){
-        if (negative){
-            percentage -= 1;
-        }
-        else{
-            percentage += 1;
-        }
-
-        if (percentage >= 100){
-            negative = true;
-        }
-        else if (percentage <= 0){
-            negative = false;
-        }
-
-        timer_led1->PWM_start(GPIO::pinMap[LED_BUILTIN_1], percentage);
-        timer_led2->PWM_start(GPIO::pinMap[LED_BUILTIN_2], percentage);
-    }
-
+    pressed = false;
 
     if (digitalRead(BUTTON_BUILTIN_1)){
         pressed = true;
@@ -83,5 +50,5 @@ void loop()
         digitalWrite(LED_BUILTIN_2, true);
     }
 
-    delay(10);
+    delay(100);
 }
