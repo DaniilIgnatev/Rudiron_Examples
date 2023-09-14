769d3d693f402ea986482d2b47b50ce269eb3faa
Daniil Ignatev <i.daniil2010@yandex.ru>
2023-09-14 17:07:29 +0200

    renamed sketch to project name

--- test.py
+++ test.py
@@ -1,12 +1,47 @@
 import os
-import build
+import shutil
+import subprocess
+
+
+def clear_cmake_project(project_abs_path: str):
+    build_path = f"{project_abs_path}/build"
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
+
+    build_debug_path = f"{project_abs_path}/build-debug"
+    if os.path.exists(build_debug_path):
+        shutil.rmtree(build_debug_path)
+
+
+def build_cmake_project(project_path: str):
+    build_path = f"{project_path}/build"
+    if not os.path.exists(build_path):
+        os.mkdir(build_path)
+
+    cmake_command = [
+        "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-G",
+        "Ninja",
+        "-S",
+        project_path,
+        "-B",
+        build_path,
+    ]
+    result_code = subprocess.call(cmake_command)
+
+    if result_code == 0:
+        cmake_command = ["cmake", "--build", build_path]
+        result_code = subprocess.call(cmake_command)
+
+    return result_code
 
 
 def traverse_directories(directory, project_handler):
     for element in os.listdir(directory):
         joined_path = os.path.join(directory, element)
         if os.path.isdir(joined_path):
-            if 'CMakeLists.txt' in os.listdir(joined_path):
+            if "CMakeLists.txt" in os.listdir(joined_path):
                 project_abs_path = os.path.abspath(path=joined_path)
                 project_handler(project_abs_path)
 
@@ -14,39 +49,43 @@ def traverse_directories(directory, project_handler):
 
 
 projects_found = 0
+
+
 def count_project(project_abs_path):
     global projects_found
 
-    print('Found sketch in ' + project_abs_path)
+    print("Found sketch in " + project_abs_path)
     projects_found += 1
 
 
 current_project_index = 0
-def build_project(project_abs_path):
+
+
+def build_cmake_project(project_abs_path):
     global projects_found
     global current_project_index
     current_project_index += 1
 
     print(f"Building the project №{current_project_index} of {projects_found} found")
-    result_code = build.build_project(project_abs_path)
+    result_code = build_cmake_project(project_abs_path)
     if result_code != 0:
-        build.clear_project(project_abs_path)
-        result_code = build.build_project(project_abs_path)
+        clear_cmake_project(project_abs_path)
+        result_code = build_cmake_project(project_abs_path)
         if result_code != 0:
-            print(f'Error compiling project: {project_abs_path}')
+            print(f"Error compiling project: {project_abs_path}")
             exit(-1)
 
 
-def clear_project(project_abs_path):
-    build.clear_project(project_abs_path)
+def clear_cmake_project(project_abs_path):
+    clear_cmake_project(project_abs_path)
 
 
-def fix_project(project_abs_path):
-    print('Found sketch in ' + project_abs_path)
-    path = os.path.join(project_abs_path, 'CMakeLists.txt')
+def fix_cmakelists(project_abs_path):
+    print("Found sketch in " + project_abs_path)
+    path = os.path.join(project_abs_path, "CMakeLists.txt")
     print(path)
     lines = []
-    with open(path, 'r', encoding='ISO-8859-1') as file:
+    with open(path, "r", encoding="ISO-8859-1") as file:
         lines = file.readlines()
         line_id = None
 
@@ -57,20 +96,97 @@ def fix_project(project_abs_path):
 
         if line_id != None:
             lines[line_id] = 'set(board_name "Rudiron_Buterbrod_R9_16MHz")'
-    
-    with open(path, 'w', encoding='ISO-8859-1') as file:
+
+    with open(path, "w", encoding="ISO-8859-1") as file:
         file.writelines(lines)
 
 
+def rename_cpp_sketch(project_abs_path):
+    print("Found sketch in " + project_abs_path)
+
+    sketch_name = os.path.basename(project_abs_path)
+    path_cpp = os.path.join(project_abs_path, sketch_name + ".cpp")
+
+    path_sketch = os.path.join(project_abs_path, "sketch.cpp")
+    if not os.path.exists(path_cpp):
+        if os.path.exists(path_sketch):
+            shutil.copy(path_sketch, path_cpp)
+
+    if os.path.exists(path_sketch):
+        os.remove(path_sketch)
+
+
+def generate_ino_project(project_abs_path):
+    print("Found sketch in " + project_abs_path)
+
+    sketch_name = os.path.basename(project_abs_path)
+    path_cpp = os.path.join(project_abs_path, sketch_name + ".cpp")
+
+    path_ino_folder = os.path.join(project_abs_path, sketch_name)
+    if not os.path.exists(path_ino_folder):
+        os.mkdir(path_ino_folder)
+
+    path_ino = os.path.join(path_ino_folder, sketch_name + ".ino")
+    if not os.path.exists(path_ino):
+        shutil.copy(path_cpp, path_ino)
+
+
+def build_ino_project(project_abs_path):
+    sketch_name = os.path.basename(project_abs_path)
+
+    path_ino_folder = os.path.join(project_abs_path, sketch_name)
+    path_ino = os.path.join(path_ino_folder, sketch_name + ".ino")
+    print("Building ino file: " + path_ino)
+    # arduino-cli compile C:\Users\Daniil\Documents\GitHub\Rudiron_Projects\basic  -v
+
+    command = [
+        "arduino-cli",
+        "compile",
+        path_ino_folder,
+        "-b",
+        "Rudiron:MDR32F9Qx:buterbrodR916",
+        "-v"
+    ]
+    result_code = subprocess.call(command)
+
+    if result_code != 0:
+        print(f"Error compiling ino project: {path_ino_folder}")
+        exit(-1)
+
+
+def clear_ino_project(project_abs_path):
+    sketch_name = os.path.basename(project_abs_path)
+    path_ino_folder = os.path.join(project_abs_path, sketch_name)
+    if os.path.exists(path_ino_folder):
+        shutil.rmtree(path_ino_folder)
+
+
 if __name__ == "__main__":
-    # traverse_directories(os.curdir, fix_project)
+    # traverse_directories(os.curdir, fix_cmakelists)
 
     traverse_directories(os.curdir, count_project)
     print(f"Found {projects_found} projects\n")
 
-    print("Started building projects")
-    traverse_directories(os.curdir, build_project)
-    print("Ended building projects")
+    # print("Started renaming projects")
+    # traverse_directories(os.curdir, rename_cpp_sketch)
+    # print("Ended renaming projects")
 
-    traverse_directories(os.curdir, clear_project)
-    print("Cleared all projects")
+    print("Started generating ino projects")
+    traverse_directories(os.curdir, generate_ino_project)
+    print("Ended generating ino projects")
+
+    # print("Started building arduino projects")
+    # traverse_directories(os.curdir, build_ino_project)
+    # print("Ended building arduino projects")
+
+    print("Started clear arduino projects")
+    traverse_directories(os.curdir, clear_ino_project)
+    print("Ended clear arduino projects")
+
+    # print("Started building cmake projects")
+    # traverse_directories(os.curdir, build_cmake_project)
+    # print("Ended buildin cmake projects")
+
+    print("Started clear cmake projects")
+    traverse_directories(os.curdir, clear_cmake_project)
+    print("Ended clear cmake projects")
